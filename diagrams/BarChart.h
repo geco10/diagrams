718da52ea95fe1e189c3ac7c01ddf8240cf8f22c
Dilena Koplande <koplande@yahoo.com>
2022-10-18 09:47:26 -0500

    almost finished barChart

--- diagrams/BarChart.h
+++ diagrams/BarChart.h
@@ -2,15 +2,23 @@
 #include<utility>
 #include<vector>
 #include<SFML/Graphics.hpp>
-class BarChart{
+class BarChart :public sf::Drawable{
 	std::vector<std::pair<double, int>> bars;
+	sf::Vector2f size;
+	sf::Vector2f loc;
+	sf::Color barsColor;
 	double width;
 public:
+	void setSize(float x, float y);
+	void drawInNewindow(int x, int y, bool sets = true);
+	void setBarsColor(int r, int g, int b, int a = 255);
 	BarChart(const std::vector<double>* arr);
-	void draw(sf::RenderTarget& target, sf::RenderStates states);
+	void draw(sf::RenderTarget& target, sf::RenderStates states)const override;
 private:
 	int mRet();
+	void countSetings(int x, int y);//false-custom setting
 	double fStep(double min,double max);
 	void makeBars(const std::vector<double>* arr);
+	int maxBar()const;
 };
 
--- diagrams/diagrams.cpp
+++ diagrams/diagrams.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include"BarChart.h"
 #include"x_axis.h"
 int main(){
 	std::vector<double> arr={4,9,-6,7,-1,10};
 	x_axis xa(&arr);
+	BarChart bar(&arr);
 	xa.drawInNewindow(800, 800);
    return 0;
 }
